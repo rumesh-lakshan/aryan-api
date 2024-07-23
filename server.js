84192728c6f64af4a425393b3884436e23df6a39
PeirisWRL <123114138+rumesh-lakshan@users.noreply.github.com>
2024-07-23 21:12:40 +0530

    commit 1 pasindu

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const path = require('path');
 const dbConnection = require("./utils/database");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: 'https://www.aryanrentacarandtours.com' }));
 app.use(express.json());
 
 const port = process.env.PORT || 5000;
 
-//import routes
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Import routes
 const userRoute = require("./routes/user/user.routes");
 const carRoute = require("./routes/vehicles/car.routes");
 const bookingRouter = require("./routes/vehicles/booking.routes");
 
-//define routes
+// Define API routes
 app.use("/api/users/", userRoute);
 app.use("/api/cars/", carRoute);
 app.use("/api/bookings/", bookingRouter);
@@ -24,7 +28,12 @@ app.get("/", (req, res) => {
   res.send("Welcome to Car renting system!");
 });
 
+// Serve frontend index.html for all other routes
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.listen(port, () => {
-  console.log(`Node JS Server Started port ${port}`);
+  console.log(`Node JS Server Started on port ${port}`);
   dbConnection.connectDB();
 });
